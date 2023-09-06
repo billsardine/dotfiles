36b3346157de790d33346d502896afc2fbfda3c3
Randy Pope <randy.pope@gmail.com>
2023-09-06 13:40:16 -0400

    updated application list

--- dotfile_script.sh
+++ dotfile_script.sh
@@ -3,68 +3,51 @@
 # sudo bash -c "echo net.inet.ip.ttl=65 >> /etc/sysctl.conf"
 # Homebrew Script for OSX setup
 echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
-# Extensions & utilities
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Extensions & utilities See notes for descriptions
 brew install tig
 brew install mdcat
 brew install python3
 brew install python
 brew install pipenv
-# brew install iperf
-# brew install iperf3
 brew install openssl
 brew install wget
 brew install bmon
-# brew install wireshark
-brew install fzf
 brew install fx
 brew install bat
-# Cask Utilities
-# brew cask install iterm2
-# Terriform install
-# brew tap hashicorp/tap
-# brew install hashicorp/tap/terraform
-# Cask Apps
-# brew cask install 1password
-# brew cask install muzzle
-# brew cask install sublime-text
-# brew cask install tunnelblick
-# brew cask install google-chrome
-# brew cask install postman
-# brew cask install cryptomator
-# brew cask install wireshark
-# brew cask install todoist
+brew install stats
+# Common Apps
+brew install --cask hiddenbar 
+brew install --cask iterm2
+brew install --cask todoist
+brew install --cask 1password
 brew install --cask adobe-acrobat-reader
-brew install --cask angry-ip-scanner
-# brew cask install vmware-remote-console
-brew install --cask  onyx
-brew install --cask  java
-brew install --cask  visual-studio-code
-# brew cask install telegram
-# Need to actually run the script to install aliases/keybindings
-/usr/local/opt/fzf/install
-# VS Code
-# Need to open VS Code so the symlinks can be created (Code folder won't exist yet)
-# code
-# Install VS Code Extensions
-# code --install-extension davidanson.vscode-markdownlint
-# code --install-extension eamodio.gitlens
-# code --install-extension pkief.material-icon-theme
-# code --install-extension zhuangtongfa.material-theme
-# code --install-extension zhuangtongfa.one-dark-pro
-# code --install-extension dotjoshjohnson.xml
-# code --install-extension ecmel.vscode-html-css
-# code --install-extension mechatroner.rainbow-csv
-# code --install-extension ms-azuretools.vscode-docker-0.8.1
-# code --install-extension ms-python.python
-# code --install-extension ms-vscode.powershell
-# code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools-1.0.4
-# code --install-extension redhat.vscode-yaml-0.5.3
-# code --install-extension coenraads.bracket-pair-colorizer
-# code --install-extension yzhang.markdown-all-in-one
-# code --install-extension pkief.material-icon-theme
-# code --install-extension zhuangtongfa.material-theme
-# code --install-extension zhuangtongfa.one-dark-pro
+brew install --cask onyx
+brew install --cask java
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask logi-options-plus
+brew install wireguard-tools
+brew install --cask slack
+brew install --cask 1password-cli
+# Work Apps
+brew install --cask logitune
+# Personal Apps
+brew install --cask focusrite-control
+brew install --cask fl-studio
+brew install --cask steam
+brew install --cask cryptomator
+# Special Install Apps
+# brew install --cask postman
+# brew install --cask wireshark
+# brew install --cask angry-ip-scanner
+# brew install --cask vmware-remote-console
+# brew install --cask inssider
+# brew install iperf
+# brew install iperf3
+# brew install wireshark
+
 # setup ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 # Install ZSH Plugins
